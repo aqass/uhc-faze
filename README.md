694fe093693a9923aad7a1a0a2a08786cbbe8535
satook.dev <satook.dev@gmail.com>
2021-02-08 08:48:35 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+"# uhc-faze" 
+"# uhc-faze" 
---
+++ src/main/java/aqass/fazeuhc/FazeUhc.java
@@ -0,0 +1,17 @@
+package aqass.fazeuhc;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class FazeUhc extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
